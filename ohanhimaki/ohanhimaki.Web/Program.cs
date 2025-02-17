2b8c65326d61befad17e174005f36f9cfbf7e0bf
Olli Hanhimäki <ohanhimaki@gmail.com>
2025-02-17 19:59:47 +0200

    siirretty sivuihin liittyvät jutut serviceen

--- ohanhimaki/ohanhimaki.Web/Program.cs
+++ ohanhimaki/ohanhimaki.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ohanhimaki.Web;
+using ohanhimaki.Web.Layout;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<BlogService>();
+builder.Services.AddScoped<PagesService>();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+// Lataa blogipostaukset ennen kuin sovellus alkaa
+var pagesService = host.Services.GetRequiredService<PagesService>();
+await pagesService.InitializeAsync();
+
+await host.RunAsync();
